0a633ad8068c175d7dc0ac12589404c2a42fecf5
uday kiran <uday.marikanti86@gamil.com>
2024-03-15 12:45:29 +0530

    new project

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello from javaScript");
+console.log("hello world");
+//window.alert("hai uday ");
+// this is comment
+let name = "uday";
+let age = 21;
+let student = false;
+
+
+document.getElementById("p1").innerHTML = "hello " + name ;
+document.getElementById("p2").innerHTML = "your age " + age
+document.getElementById("p3").innerHTML = "enroll  " + false;
